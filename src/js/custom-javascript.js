2277b7438d4abd6012a071f40f25c06c0d1d3b5d
Maciej <mocador23@o2.pl>
2021-08-16 13:46:50 +0200

    Add Scroll Up function

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,1 +1,12 @@
 // Add your JS customizations here
+var btnTop = document.getElementById("btn-top");
+var rootElement = document.documentElement;
+function scrollToTop() {
+	// Scroll to top logic
+	rootElement.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+}
+
+btnTop.addEventListener("click", scrollToTop);
