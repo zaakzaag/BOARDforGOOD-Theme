aa895a38f436bf6e938d63085a511d32d1618e88
Maciej <mocador23@o2.pl>
2021-08-17 12:04:22 +0200

    Fix-8: Contact section as footer

--- footer.php
+++ footer.php
@@ -9,11 +9,57 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-
+$section_title = get_field('section_title','option');
+$privacy_policy = get_field('privacy_policy','option');
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+
+<section class="section contact-section pb-5" id="Kontakt">
+
+    <div class="container-fluid ">
+
+        <div class="row d-flex align-items-start justify-content-center ">
+
+            <div class="col-lg-7 col-md-10 col-sm-11 col-12 d-flex">
+                <h2><?php echo $section_title ?></h2>
+            </div>
+        </div>
+
+
+
+        <?php if( have_rows('address','option') ): ?>
+            <?php while( have_rows('address','option') ): the_row(); 
+                                    $address_data = get_sub_field('address_data');
+                                    ?>
+                <div class="row d-flex align-items-start justify-content-center py-2 ">
+
+                    <div class="col-lg-7 col-md-10 col-sm-11 col-12 d-flex">
+                        <address>
+                            <?php echo $address_data ?>
+                        </address>
+                    </div>
+                </div>
+            <?php endwhile; ?>  
+        <?php endif; ?>
+
+
+        <div class="go-to-top d-flex justify-content-between align-items-center pt-4 pb-2 mx-4">
+            <div class="privacy-policy">
+                <?php if ($privacy_policy): ?>
+                    <a href="<?php echo $privacy_policy['privacy_policy_link'] ?>"><?php echo $privacy_policy['privacy_policy_title'] ?></a>
+                <?php endif; ?>
+            </div>
+            <div id="btn-top" class="go-top-btn btn btn-outline-dark d-flex justify-content-center align-items-center ">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/icon-arrow-top.svg" > 
+                Top
+            </div>
+        </div>
+    
+    </div>
+</section>
 
 
 </div><!-- #page we need this extra closing tag here -->
--- functions.php
+++ functions.php
@@ -132,3 +132,22 @@ function add_search_form($items, $args) {
   return $items;
 }
 add_filter('wp_nav_menu_items', 'add_search_form', 10, 2);
+
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+}
